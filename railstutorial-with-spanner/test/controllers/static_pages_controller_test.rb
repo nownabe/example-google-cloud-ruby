9c24761a39be1cc58eebab20a84a0ce21c97a4d6
nownabe <nownabe@users.noreply.github.com>
2022-11-24 14:43:51 +0900

    Chapter 4

--- railstutorial-with-spanner/test/controllers/static_pages_controller_test.rb
+++ railstutorial-with-spanner/test/controllers/static_pages_controller_test.rb
@@ -6,15 +6,15 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url 
+    get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
